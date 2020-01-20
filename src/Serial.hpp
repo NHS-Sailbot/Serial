6f4a10e1f7f983fd727d55935f4e13ef88d8b1b5
GabeRundlett <gabe@rundlett.com>
2020-01-20 17:09:56 -0500

    Removed installation and debug lib dep

--- src/Serial.hpp
+++ src/Serial.hpp
@@ -1,24 +1,26 @@
 #pragma once
 
 namespace Henry {
-    class Serial {
-      private:
-        enum Flags { NONE = 0, OPEN_STATUS = 1, VALID_STATUS = 2 };
-        unsigned int mBaudrate;
-        unsigned short mID, mFlags;
-        const char *mFilepath;
+	class SerialDevice {
+	  private:
+		enum Flags { NONE = 0,
+			OPEN_STATUS = 1,
+			VALID_STATUS = 2 };
+		unsigned int mBaudrate;
+		unsigned short mId, mFlags;
+		const char *mFilepath;
 
-      public:
-        Serial();
-        Serial(const char *const, const unsigned int);
-        ~Serial();
+	  public:
+		SerialDevice();
+		SerialDevice(const char *const deviceFilepath, const unsigned int baudrate);
+		~SerialDevice();
 
-        bool Open(const char *const, const unsigned int);
-        void Close();
-        void Flush();
-        void Read(void *const, const unsigned int);
-        void Write(void *const, const unsigned int);
-        bool IsOpen() const { return mFlags & OPEN_STATUS; }
-        bool IsValid() const { return mFlags & VALID_STATUS; }
-    };
+		bool open(const char *const deviceFilepath, const unsigned int baudrate);
+		void close();
+		void flushBuffer() const;
+		void readBuffer(void *const, const unsigned int);
+		void writeBuffer(void *const, const unsigned int);
+		bool isOpen() const { return mFlags & OPEN_STATUS; }
+		bool isValid() const { return mFlags & VALID_STATUS; }
+	};
 } // namespace Henry
